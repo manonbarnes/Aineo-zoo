8e45537a24572ea221f3d6bf248b2a83e8b603c6
Manon Barnes <manonbarnes@icloud.com>
2021-03-24 12:52:25 +0100

    added style to show

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,10 +1,10 @@
 class Animal < ApplicationRecord
   validates :name, presence: true
-  # validates :description, presence: true
-  # validates :age, presence: true
-  # validates :sex, presence: true
-  # validates :category, presence: true
-  # validates :personality_attributes, presence: true
+  validates :description, presence: true
+  validates :age, presence: true
+  validates :sex, presence: true
+  validates :category, presence: true
+  #validates :personality_attributes, presence: true
   belongs_to :user
   has_one_attached :photo
   has_many :bookings, through: :user
