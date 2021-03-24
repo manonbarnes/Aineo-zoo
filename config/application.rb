9c83c409febcb4854bd3afeed5c210af08d85253
Nicolas Oederlin <n.oederlin@gmail.com>
2021-03-24 13:39:34 +0100

    background video

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,15 @@ Bundler.require(*Rails.groups)
 
 module AineoZoo
   class Application < Rails::Application
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
     config.generators do |generate|
       generate.assets false
       generate.helper false
       generate.test_framework :test_unit, fixture: false
+
     end
+
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
